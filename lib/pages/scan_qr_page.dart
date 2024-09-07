9215eb56410e8f4e3deb24abc6d75ea5136e27e1
Nicolen Evanz Aricayos <nicolenevanz.aricayos@gmail.com>
2024-09-07 22:38:17 +0800

    created pages dir

---
+++ lib/pages/scan_qr_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScanCodePage extends StatefulWidget {
+  const ScanCodePage({super.key});
+
+  @override
+  State<ScanCodePage> createState() => _ScanCodePageState();
+}
+
+class _ScanCodePageState extends State<ScanCodePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
